b753e50b2edba38fb298c5d6283a6df0dc090356
Paul Hart <jphart@glentech.com>
2020-12-30 16:56:53 -0800

    More Updates

--- Data/C19CollectData.sh
+++ Data/C19CollectData.sh
@@ -6,19 +6,19 @@ echo -e $(date) 'Python version'
 /usr/local/bin/python3 --version
 
 echo -e $(date) 'Removing old data'
-cd /Users/paulhart/CovidProjects/Data
+cd /Users/paulhart/Development/CovidProjects/Data
 rm -rf COVID-19
 
 echo -e $(date) 'Cloning data'
 git clone https://github.com/CSSEGISandData/COVID-19.git
 
 echo -e $(date) 'Creating csv data'
-/usr/local/bin/python3 /Users/paulhart/CovidProjects/Apps/C19DataCollection/C19CollectDataMain.py
+/usr/local/bin/python3 /Users/paulhart/Development/CovidProjects/Apps/C19CollectData/C19CollectDataMain.py
 echo -e  $(date) 'Creating csv data complete'
 echo 
 
 echo -e "Add files"
-cd /Users/paulhart/CovidProjects
+cd /Users/paulhart/Development/CovidProjects
 git add --all
 
 echo 
