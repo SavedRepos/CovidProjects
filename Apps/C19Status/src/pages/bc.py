f6414a8b371eab48469ed32a3fe0f48ddd2d754a
Paul Hart <jphart@glentech.com>
2021-01-12 21:02:11 -0800

    Daily update Tue Jan 12 21:02:11 PST 2021

--- Apps/C19Status/src/pages/bc.py
+++ Apps/C19Status/src/pages/bc.py
@@ -18,11 +18,58 @@ import app
 # pylint: disable=line-too-long
 def write():
     """Used to write the page in the app.py file"""
-    with st.spinner("Loading Home ..."):
-        #ast.shared.components.title_awesome("and so it goes...")
-        st.title("British Columbia Covid Stats")
+    st.title("British Columbia Covid Stats")
     casesByHA()
 
+def casesByHA1():
+
+    # Create dataframe with all records
+    df = pd.read_csv(app.BC_REGIONAL_URL)
+    df = df.drop(columns=['Cases_Reported_Smoothed'])
+    df = df.sort_values(by=['Date', 'HA', 'HSDA'], ascending=[False, True, True])
+
+    # Create dataframe with records fromlast 7 days
+    last_date = df.Date.values[0]
+    first_date = (datetime.datetime.strptime(last_date, '%Y-%m-%d') - timedelta(days = 7)).strftime('%Y-%m-%d')
+    dfw = df[df['Date'] > first_date]
+
+    # Group by HA and HSDA
+    dfg = pd.DataFrame(df.groupby(['HA', 'HSDA'], as_index=False).sum())
+    dfw = pd.DataFrame(dfw.groupby(['HA', 'HSDA'], as_index=False).sum())
+
+    # Merge into a single dataframe
+    df  = dfw.merge(dfg, left_on=['HA','HSDA'], right_on=['HA','HSDA'])
+
+    # Table of details for last week 
+    table_rows =  '<div style="font-size: 9pt">\n'
+
+    table_rows += '<table border=1 cellspacing=0 cellpadding=0>\n'
+    table_rows += '<tr><th>Health Authority</th><th>Heath Services Delivery Area</th><th colspan=2 style="text-align:center">Cases</th></tr>\n'
+    table_rows += '<tr><th></th><th></th><th>Last 7 Days</th><th>Cases Total</th></tr>\n'
+
+    last_ha = ''
+    for index, row in df.iterrows():
+        ha = row['HA']
+        if ha == last_ha:
+            display_ha = ''
+        else:
+            display_ha = f'<b>{ha}</b>'
+        if ha == 'All':
+            ha = 'Total'
+        hsda = row['HSDA']
+        if hsda == 'All':
+            hsda = 'Total'
+        casesX = "{:,}".format(row['Cases_Reported_x'])
+        casesY = "{:,}".format(row['Cases_Reported_y'])
+        table_row = f'<tr><td>{display_ha}</td><td>{hsda}</td><td style="text-align:right">{casesX}</td><td style="text-align:right">{casesY}</td></tr>\n'
+        table_rows += table_row
+        last_ha = ha
+
+    table_rows += '</table>\n'
+    table_rows += '</div>\n'
+    
+    st.markdown('#### BCCDC Cases by Region')
+    st.markdown(table_rows, unsafe_allow_html=True)
 
 def casesByHA():
 
@@ -54,10 +101,9 @@ def casesByHA():
     for index, row in df.iterrows():
         ha = row['HA']
         if ha == last_ha:
-            ha = ''
+            display_ha = ''
         else:
-            #ha = f'<b>{ha}</b>'
-            last_ha = ha
+            display_ha = f'<b>{ha}</b>'
         if ha == 'All':
             ha = 'Total'
         hsda = row['HSDA']
@@ -65,8 +111,9 @@ def casesByHA():
             hsda = 'Total'
         casesX = "{:,}".format(row['Cases_Reported_x'])
         casesY = "{:,}".format(row['Cases_Reported_y'])
-        table_row = f'<tr><td cellspacing=1 cellpadding=1>{ha}</td><td cellspacing=1 cellpadding=1>{hsda}</td><td style="text-align:right">{casesX}</td><td style="text-align:right" cellspacing=1 cellpadding=1>{casesY}</td></tr>\n'
+        table_row = f'<tr><td>{display_ha}</td><td>{hsda}</td><td style="text-align:right">{casesX}</td><td style="text-align:right">{casesY}</td></tr>\n'
         table_rows += table_row
+        last_ha = ha
 
     table_rows += '</table>\n'
     table_rows += '</div>\n'
