1975f71ca76bfd922d0421ab4bbc660192e59f77
Paul Hart <jphart@glentech.com>
2021-01-16 16:58:25 -0800

    Daily update Sat Jan 16 16:58:25 PST 2021

--- Apps/C19Status/src/pages/vaccinations.py
+++ Apps/C19Status/src/pages/vaccinations.py
@@ -23,3 +23,20 @@ def write():
     """Used to write the page in the app.py file"""
     st.title("Vaccinations by Day")
     st.markdown('#### ')
+
+# Canada Vaccination stats
+# "date_vaccine_administered","province","cumulative_avaccine"
+# CANADA_VACCINATION_ADMINSTERED = 'https://raw.githubusercontent.com/ccodwg/Covid19Canada/master/vaccine_administration_cumulative.csv'
+# "date_vaccine_distributed","province","cumulative_dvaccine"
+# CANADA_VACCINATION_DISTRIBUTED = 'https://raw.githubusercontent.com/ccodwg/Covid19Canada/master/vaccine_distribution_cumulative.csv'
+
+    dfAdmin = pd.read_csv(cn.CANADA_VACCINATION_ADMINSTERED)
+    dfAdmin = dfAdmin.sort_values(['date_vaccine_administered'], ascending=[True])
+    dfAdmin['cumulative_avaccine_mean'] = dfAdmin['cumulative_avaccine'].rolling(7).mean()
+    print(dfAdmin)
+
+    dfDistr = pd.read_csv(cn.CANADA_VACCINATION_DISTRIBUTED)
+    dfDistr = dfDistr.sort_values(['date_vaccine_distributed'], ascending=[True])
+    dfDistr['cumulative_dvaccine_mean'] = dfDistr['cumulative_dvaccine'].rolling(7).mean()
+    print(dfDistr)
+
